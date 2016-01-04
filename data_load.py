0803c8440a57d61603c9821272fc1a70e8676aaf
SnowMasaya <snow.akogi.pgel@gmail.com>
2016-01-04 11:45:00 +0900

    add data load python

---
+++ data_load.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import json
+
+f = open("list", "r")
+player_1 = open("player_1", "w")
+player_2 = open("player_2", "w")
+count = 0
+
+for line in f.readlines():
+    f = open(line.rstrip())
+    data = json.load(f)
+    for i in range(len(data["turns"])):
+        if i % 2 == 1:
+            player_1.write(data["turns"][i]["utterance"] + "\n")
+        else:
+            if i == 0:
+                print("pass")
+            else: 
+                player_2.write(data["turns"][i]["utterance"] + "\n")
