53f8dcc8a25c6b09e43892e11de10c885d9628eb
SnowGushiGit <snow.akogi.pgel@gmail.com>
2016-05-05 14:51:03 +0900

    twitter

---
+++ twitter/sqlite_twitter_cython.pyx
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#!/usr/bin/python3
+from __future__ import division
+
+import sqlite3
+import MeCab
+import yaml
+from collections import namedtuple
+import operator
+import os
+
+
+class SqliteTwitterCython(object):
+    """
+    Twitter Save to the SQLite
+    """
+    def __init__(self, class_word_vector):
+        """
+        Initial Setting
+        Get the mecab dict by the yaml
+        """
+        Twitter = namedtuple("Twitter", ["mecab"])
+        config_file = "enviroment.yml"
+
+        with open(config_file, encoding="utf-8") as cf:
+            e = yaml.load(cf)
+            twitter = Twitter(e["twitter"]["mecab"])
+
+        self.tagger = MeCab.Tagger("-Owakati -d %s" % twitter.mecab)
+        conn = sqlite3.connect('./twitter_data.db')
+        self.cur = conn.cursor()
+        self.class_word_vector = class_word_vector
+        self.class_word_dict = {}
+        for class_name, word_list in self.class_word_vector.items():
+            class_word_dict = {}
+            for word in word_list:
+                if word not in class_word_dict:
+                    class_word_dict.update({word: 1})
+            if class_name not in self.class_word_dict:
+                self.class_word_dict.update({class_name: class_word_dict})
+
+    def call_sql(self):
+        """
+        call SQlite and save the twitter in the SQLite
+        """
+        self.cur.execute("""SELECT source_txt, replay_txt FROM ms_rinna;""")
+        file_list = os.listdir("./data/")
+        for file in file_list:
+            os.remove("./data/" + file)
+        for source_txt, replay_txt in self.cur.fetchall():
+            class_name = self.__judge_class(source_txt, replay_txt)
+            print(class_name)
+            print(source_txt)
+            print(replay_txt)
+            source_file = open("./data/" + class_name + '_source_twitter_data.txt', 'a')
+            replay_file = open("./data/" + class_name + '_replay_twitter_data.txt', 'a')
+            replay_file.write(self.tagger.parse(source_txt).replace("\n", "") + '\n')
+            source_file.write(self.tagger.parse(replay_txt).replace('\n', '') + '\n')
+            source_file.close()
+            replay_file.close()
+
+    def __judge_class(self, source_txt, replay_txt):
+        """
+        Judge word class
+        :param source_txt: twitter source text
+        :param replay_txt: twitter replay text
+        :return: most match class
+        """
+        class_match_rate = {}
+        source_wakati_text = self.__mecab_method(source_txt.strip())
+        replay_wakati_text = self.__mecab_method(replay_txt.strip())
+        total_text = []
+        total_text.extend(source_wakati_text)
+        total_text.extend(replay_wakati_text)
+        for class_name in self.class_word_vector.keys():
+            word_match_count = self.__match_word_count(total_text, class_name)
+            if class_name not in class_match_rate:
+                class_match_rate.update({class_name: 1.0 * word_match_count / len(self.class_word_dict[class_name])})
+        if max(class_match_rate.values()) == 0.0:
+            return "other"
+        else:
+            return max(class_match_rate.items(), key=operator.itemgetter(1))[0]
+
+    def __mecab_method(self, text):
+        """
+        Call Mecab method split process and choose noum
+        :param text:
+        :return: only noum
+        """
+        res = self.tagger.parseToNode("".join(text))
+        split_nonum = []
+        while res:
+            feature = res.feature.split(",")
+            if feature[0] == u"名詞":
+                split_nonum.append(feature[6])
+            res = res.next
+        return split_nonum
+
+    def __match_word_count(self, total_text, class_name):
+        word_match_count = 0
+        for word in total_text:
+            if word in self.class_word_dict[class_name]:
+                if self.class_word_dict[class_name][word]:
+                    word_match_count = word_match_count + 1
+        return word_match_count
---
+++ twitter/test/test_twitter_summary_cython.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+import pyximport
+pyximport.install()
+from sqlite_twitter_cython import SqliteTwitterCython
+from split_data.input_file_cython import InputFileCython
+from os import path
+APP_ROOT = path.dirname(path.abspath(__file__))
+import re
+
+class Test_ClassSummaryCosineSimilarty(unittest.TestCase):
+    """Test Class Summary class.
+
+    """
+    def setUp(self):
+        """
+        setting initial paramater
+        Args:
+            data: test file name
+            split_module: setting the split_module instance
+        """
+        wn_summary_list = APP_ROOT + '/../../Data/wn_total_summary_list.txt'
+        self.input_module = InputFileCython(wn_summary_list)
+        self.input_module.input_special_format_file()
+
+    def test_summary_class(self):
+        """
+        test make summary dict
+        """
+        file_list = self.input_module.get_file_data()
+        class_word_vector = {}
+        for file in file_list:
+            self.input_module = InputFileCython(APP_ROOT + "/../../Data/wn_summary/" + file.strip())
+            self.input_module.input_special_format_file()
+            if file.strip() not in class_word_vector:
+                word_list = re.sub("\]|\[|\'", "", self.input_module.get_file_data()[0].strip())
+                class_word_vector.update({file.strip().replace(".txt", ""): word_list.split(",")})
+        sqlite_twitter_cython = SqliteTwitterCython(class_word_vector)
+        sqlite_twitter_cython.call_sql()
+
+
+if __name__ == '__main__':
+    unittest.main()
