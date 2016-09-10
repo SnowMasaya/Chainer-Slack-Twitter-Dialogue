56f5379bf44cca710013645bc0c5f864b4707d9c
SnowGushiGit <snow.akogi.pgel@gmail.com>
2016-09-10 11:39:06 +0900

    move docker
    for clearly to understand which directory mean

--- docker/shell/docker_rm_img_none.sh
+++
@@ -1,3 +0,0 @@
-docker stop `docker ps -a -q`
-docker rm `docker ps -a -q`
-docker rmi $(docker images | awk '/^<none>/ { print $3 }')
--- docker/template/python/pyenv.sh
+++
@@ -1,4 +0,0 @@
-export PYENV_ROOT="/usr/local/pyenv"
-export PATH="${PYENV_ROOT}/bin:${PATH}"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
